d38399cf9d4c761320f9914da500c17a15556b7b
unknown <uchitha.r@gmail.com>
2014-06-11 21:58:40 +0800

    initial commit modified files

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,9 @@ angular.module( 'ngBoilerplate', [
   'templates-common',
   'ngBoilerplate.home',
   'ngBoilerplate.about',
+  'ngBoilerplate.pc',
+  'ngBoilerplate.media',
+  'ngBoilerplate.software',
   'ui.router'
 ])
 
@@ -16,7 +19,7 @@ angular.module( 'ngBoilerplate', [
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
+      $scope.pageTitle = toState.data.pageTitle + ' | TNU ';
     }
   });
 })
